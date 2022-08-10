9a3a54fff0123d91f77b1cc667232d639760c50e
patricia-oliveira <patriciia.mota@gmail.com>
2022-08-10 21:45:58 +0100

    Remove unecessary tests

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,10 +1,8 @@
 import App from './App';
-import {create} from 'react-test-renderer';
 
 describe('<App />', () => {
 
-  it('should match with snapshot', function () {
-    const app = create(<App />)
-    expect(app.toJSON()).toMatchSnapshot()
+  it('', function () {
+
   });
 });
