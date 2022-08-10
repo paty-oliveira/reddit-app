1d1ddfcc49599c72f930f96bb12193fb84d022b7
patricia-oliveira <patriciia.mota@gmail.com>
2022-08-10 19:26:50 +0100

    Add SearchBar component

---
+++ src/search-bar/SearchBar.js
@@ -0,0 +1,18 @@
+import './SearchBar.css';
+
+export function SearchBar () {
+
+    return (
+        <div className="search-bar-container">
+            <form>
+                <input
+                    type="text"
+                    id="searchBar"
+                    name="searchBar"
+                    value="Search Reddit posts"
+                />
+                <button id="search-button" type="submit">Search</button>
+            </form>
+        </div>
+    )
+}
