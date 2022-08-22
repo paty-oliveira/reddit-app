7ce5842f9df26d2ab26378f3a004364b354410a1
patricia-oliveira <patriciia.mota@gmail.com>
2022-08-22 11:19:22 +0100

    Add initial implementation of PostsContainer component

--- src/app/App.js
+++ src/app/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import './App.css';
 import { Header } from "../header/Header";
 import { SubredditContainer } from "../subreddit-container/SubredditContainer";
+import { PostsContainer } from "../posts-container/PostsContainer";
 
 function App() {
 
     return (
         <div className='App'>
             <Header />
-            <SubredditContainer />
+            <div className="app-container">
+                <SubredditContainer />
+                <PostsContainer />
+            </div>
         </div>
   );
 }
---
+++ src/posts-container/PostsContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./PostsContainer.css";
+
+export function PostsContainer() {
+    return (
+        <div className="post-container">
+            <p>Post 1</p>
+            <p>Post 1</p>
+        </div>
+    )
+}
