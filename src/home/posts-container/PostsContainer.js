b066f050be531bbb786683e336778aa4c963c6d8
patricia-oliveira <patriciia.mota@gmail.com>
2022-08-22 12:05:00 +0100

    Add initial implementation of Post component

--- src/home/posts-container/PostsContainer.js
+++ src/home/posts-container/PostsContainer.js
@@ -4,8 +4,20 @@ import "./PostsContainer.css";
 export function PostsContainer() {
     return (
         <div className="post-container">
-            <p>Post 1</p>
-            <p>Post 1</p>
+            <div className="post-item">
+                <div className="buttons-container">
+                    <button>Like</button>
+                    <button>Dislike</button>
+                </div>
+                <div className="post-body" >
+                    <h3 className="post-title">Title of the post</h3>
+                    <div className="post-details">
+                        <p>Posted by author1</p>
+                        <p>At 1 hour ago</p>
+                        <p>250 comments</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
