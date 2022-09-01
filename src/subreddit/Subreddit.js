e6075c01d8b9ad09469b4e3400e8f84f7e6495d3
patricia-oliveira <patriciia.mota@gmail.com>
2022-09-01 18:50:03 +0100

    Dispatch setSubredditSelected action when click on subreddit button

--- src/subreddit/Subreddit.js
+++ src/subreddit/Subreddit.js
@@ -1,15 +1,25 @@
 import React from "react";
 import "./Subreddit.css";
+import { useDispatch } from "react-redux";
+import {setSubredditSelected} from "../home/posts-container/postsSlice";
 
 
 export function Subreddit({title, url}) {
+
+    const dispatch = useDispatch();
+
+    const handleSubredditClick = (event) => {
+        dispatch(setSubredditSelected(url))
+    }
 
     return (
         <div className="subreddit-item" >
             <button
                 type="button"
                 className="subreddit-button"
-                data-testid="test-title-props">{title}
+                data-testid="test-title-props"
+                onClick={handleSubredditClick}
+            >{title}
             </button>
         </div>
     )
