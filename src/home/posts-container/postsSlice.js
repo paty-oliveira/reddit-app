be7c7e7454fdf7f62c15387afe9d51ef73289b0e
patricia-oliveira <patriciia.mota@gmail.com>
2022-09-01 18:40:20 +0100

    Add setSubredditSelected reducer to update subredditSelected

--- src/home/posts-container/postsSlice.js
+++ src/home/posts-container/postsSlice.js
@@ -2,16 +2,23 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     posts: [],
-    subredditSelected: "r/Home/"
+    subredditSelected: "r/Home/",
+    error: false,
+    loading: false
 };
 
 const options = {
     name: "posts",
     initialState,
     reducers: {
+        setSubredditSelected(state, action) {
+            state.subredditSelected = action.payload;
+        }
     }
 }
 
 export const postsSlice = createSlice(options);
+
+export const { setSubredditSelected } = postsSlice.actions;
 
 export default postsSlice.reducer;
--- src/home/posts-container/postsSlice.test.js
+++ src/home/posts-container/postsSlice.test.js
@@ -1,4 +1,4 @@
-import reducer, { postsSlice } from "./postsSlice";
+import reducer, { postsSlice, setSubredditSelected } from "./postsSlice";
 
 describe("postsSlice reducer", () => {
 
@@ -6,11 +6,33 @@ describe("postsSlice reducer", () => {
         const currentState = reducer(undefined, {});
         const expectedState = {
             posts: [],
-            subredditSelected: "r/Home/"
+            subredditSelected: "r/Home/",
+            error: false,
+            loading: false
         };
 
         expect(currentState).toEqual(expectedState)
     });
 
+    it('should update subredditSelected when a new subreddit is selected', function () {
+        const initialState = {
+            posts: [],
+            subredditSelected: "r/Home/",
+            error: false,
+            loading: false
+        };
 
+        const payload = "r/pics/"
+
+        const expectedState = {
+            posts: [],
+            subredditSelected: payload,
+            error: false,
+            loading: false
+        };
+
+        const actualState = reducer(initialState, setSubredditSelected(payload))
+
+        expect(actualState).toEqual(expectedState)
+    });
 })
